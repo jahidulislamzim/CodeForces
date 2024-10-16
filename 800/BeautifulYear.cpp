6f46c855fe6d77a88c885c41c0097895b1ee9eb6
jahidulislamzim <jahidulislamzim845@gmail.com>
2024-10-16 23:10:52 +0600

    adding feautiful year half

---
+++ 800/BeautifulYear.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int year, temp;
+    cin >> year;
+    temp = year;
+    bool isValid = true;
+
+    while(isValid){
+        temp += 1;
+        bool is
+
+        if(temp > year){
+           
+            cout << temp << endl;
+            isValid = false;
+        }
+        // for(int i=0; i<4; i++){
+            
+        // }
+        
+    }
+
+
+
+
+    return 0;
+}
